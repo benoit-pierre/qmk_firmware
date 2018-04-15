8bfcb991c22e517bba230f28a358cbab101cec03
Benoit Pierre <benoit.pierre@gmail.com>
2018-04-15 14:03:38 +0200

    preonic/bpierre: enable MIDI steno support
    Note: audio had to be disabled to reduce the resulting firmware size.

--- keyboards/preonic/keymaps/bpierre/rules.mk
+++ keyboards/preonic/keymaps/bpierre/rules.mk
@@ -2,11 +2,11 @@ ifndef QUANTUM_DIR
 	include ../../../../Makefile
 endif
 
-AUDIO_ENABLE = yes          # Audio output on port C6
+AUDIO_ENABLE = no           # Audio output on port C6
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 CONSOLE_ENABLE = no         # Console for debug(+400)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-MIDI_ENABLE = no            # MIDI controls
+MIDI_ENABLE = yes           # MIDI controls
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 STENO_ENABLE = no           # Additional protocols for Stenography(+1700), requires VIRTSER
